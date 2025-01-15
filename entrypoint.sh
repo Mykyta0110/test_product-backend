6f556c5bc771e074ee853c41edadf16998a4fb75
Mykyta0110 <mykyta603@gmail.com>
2025-01-15 13:34:40 +0200

    update docker start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-
+# Очікуємо, поки база даних стане доступною (якщо потрібно, але Render дасть вам правильний URL)
 echo "Waiting for the database to be ready..."
-until nc -z -v -w30 db_products 5432; do
-  echo "Waiting for db_products to be available..."
+
+# Якщо ви використовуєте Alembic для міграцій, перевірте, чи підключається до правильного URL
+until alembic upgrade head; do
+  echo "Waiting for database to be available for migrations..."
   sleep 1
 done
 
-
-echo "Running Alembic migrations..."
-alembic upgrade head
-
-
+# Запускаємо FastAPI додаток
 exec uvicorn main:app --host 0.0.0.0 --port 8000
--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ env = Env()
 
 REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
-    default="postgresql+asyncpg://postgres:Admin@db_products:5432/products"
+    default="postgresql+asyncpg://postgres_user:rk1vDwkXLJGgl7cPE8nxt2nXyyxDyoBE@dpg-cu3pjid6l47c73a962eg-a.oregon-postgres.render.com/products_ntit"
 )
 
 APP_PORT = env.int("APP_PORT", default=8000)
